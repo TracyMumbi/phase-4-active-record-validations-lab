72c5c88825429569ebb7c437e6cc4738f6594812
Tracy Mumbi <tracychiuri@gmail.com>
2023-03-16 23:34:32 +0300

    Completed Assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {in: 250..500}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: %w(Fiction Non-Fiction)} 
+    validate :title_clickbait
+
+    def title_clickbait
+        if title == "True Facts"
+            errors.add(:title, "Invalid title")
+        else
+        end
+    end
 end
